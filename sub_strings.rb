b035aaf24dae0ae39dfacbe0a8736a5c684cfba8
hbhp <114692427+hbhp@users.noreply.github.com>
2023-03-20 10:33:59 +0000

    Finish method
    Create .each loop for dictionary to collect length of array created using .scan

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,10 +1,14 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(word, dictionary)
-sub_hash ={}
-hows = 0
-word_to_array = word.downcase.split(/[[:punct:]]/).map{|str|str.split}.flatten
-p word_to_array
+  sub_hash = Hash.new(0)
+  word_down = word.downcase
+  
+  dictionary.each do |w| 
+    matches = word_down.scan(w).length
+    sub_hash[w] = matches unless matches == 0
+  end
+  return sub_hash
 end
 
 substrings("Howdy partner, sit down! How's it going?", dictionary)
